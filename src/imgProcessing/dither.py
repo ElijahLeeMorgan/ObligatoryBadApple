a33a03d78f36603b1adf770af182e24f10a17f19
Elijah Morgan <morgaeli@mail.gvsu.edu>
2025-07-21 02:46:01 +0000

    Update texture dithering

--- src/imgProcessing/dither.py
+++ src/imgProcessing/dither.py
@@ -1,8 +1,10 @@
 from PIL import Image
 from os import path, listdir
+import os
 
-INPUT_FOLDER = "../../textures/dice/vanilla/"
-OUTPUT_FOLDER = "../../textures/dice/dithered/"
+INPUT_FOLDER = "/workspaces/ObligatoryBadApple/textures/dice/vanilla"
+OUTPUT_FOLDER = "/workspaces/ObligatoryBadApple/textures/dice/dithered"
+SIZE = (40, 40)
 
 for img_path in listdir(INPUT_FOLDER):
     with Image.open(path.join(INPUT_FOLDER, img_path)) as img:
@@ -10,9 +12,13 @@ for img_path in listdir(INPUT_FOLDER):
         input_image = img
 
         # Resize the image to 16x16 using the Lanczos filter
-        resized_image = input_image.resize((16, 16), Image.LANCZOS)
-
+        resized_image = input_image.resize(SIZE, Image.LANCZOS)
         # Convert the image to a palette-based image with dithering
         dithered_image = resized_image.convert("P", dither=Image.FLOYDSTEINBERG)
 
-        dithered_image.save(path.join(OUTPUT_FOLDER, img_path))
+        # Ensure the output folder exists
+        os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+
+        # Save with the prefix in the filename within the output directory
+        output_path = path.join(OUTPUT_FOLDER, f"dithered_{SIZE[0]}x{SIZE[1]}_{img_path}")
+        dithered_image.save(output_path)
